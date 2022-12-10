00654cf4f42c27048890d943d75366cc4bbc00f4
Fulxe <muruuuu8@gmail.com>
2022-12-10 09:55:56 +0800

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Logo from "./jordan.png"
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="First">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <div className="navbar-up">
+          <div className="navbar-up-left">
+          <img src={Logo}/>
+          </div>
+          <div className="navbar-up-right">
+            <div>Find a store</div>
+            <div>Help</div>
+            <div>Join us</div>
+            <div className="nv-text">Sign in</div>
+          </div>
+        </div>
+      <div className="navbar-down">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
